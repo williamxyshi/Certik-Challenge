956dd7aa57be2787c9fe891dba25f4e45de866b7
williamxyshi <williamxyshi@gmail.com>
2021-06-02 17:43:04 -0400

    reword

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -27,7 +27,7 @@ export default function Dropdown(props) {
         <>
           <div>
             <Menu.Button className="inline-flex justify-center w-full rounded-md border border-green-900 shadow-sm px-4 py-2 bg-white text-sm font-medium text-green-700 hover:bg-green-300" >
-              Sort By: {getSortString(props.sortDate)}
+              Filter By: {getSortString(props.sortDate)}
               <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
             </Menu.Button>
           </div>
